9b0ff86a1490b173c91724bd321eb85f1c524fa6
Bytezero_zhengLei <420498246@qq.com>
2023-02-17 15:30:05 +0800

    //add author

--- app/src/main/java/com/permissionx/permissionx/MainActivity.kt
+++ app/src/main/java/com/permissionx/permissionx/MainActivity.kt
@@ -2,6 +2,14 @@ package com.permissionx.permissionx
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+/**
+ * @Author: Bytezero_zhengLei
+ * @Time: 2023/2/17 15:29
+ * @Project_Name: MainActivity.kt
+ * @Email: 420498246@qq.com
+ * @Description:
+ * @TODO:   权限库
+ */
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
